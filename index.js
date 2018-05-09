aecfd0631f48351d1ae919e7610d980f73232c6d
Daniel Araújo <daniel.araujorn01@gmail.com>
2018-05-09 16:57:11 -0300

    get data from sensors

--- index.js
+++ index.js
@@ -3,52 +3,6 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var { Layer, Network, Trainer } = require("synaptic");
 var Blob = require("blob");
-// var trainingSet = [
-//   {
-//     input: [0 / 1024, 200 / 1024],
-//     output: [1]
-//   },
-//   {
-//     input: [100 / 1024, 200 / 1024],
-//     output: [1]
-//   },
-//   {
-//     input: [200 / 1024, 200 / 1024],
-//     output: [1]
-//   },
-//   {
-//     input: [300 / 1024, 200 / 1024],
-//     output: [1]
-//   },
-//   {
-//     input: [400 / 1024, 200 / 1024],
-//     output: [1]
-//   },
-//   {
-//     input: [500 / 1024, 200 / 1024],
-//     output: [0]
-//   },
-//   {
-//     input: [600 / 1024, 200 / 1024],
-//     output: [0]
-//   },
-//   {
-//     input: [700 / 1024, 200 / 1024],
-//     output: [0]
-//   },
-//   {
-//     input: [800 / 1024, 200 / 1024],
-//     output: [0]
-//   },
-//   {
-//     input: [900 / 1024, 200 / 1024],
-//     output: [0]
-//   },
-//   {
-//     input: [1024 / 1024, 200 / 1024],
-//     output: [0]
-//   }
-// ];
 function MLP(input, hidden, output) {
   // create the layers
   var inputLayer = new Layer(input);
@@ -84,7 +38,7 @@ function getValue(analog, d) {
 var myNet = new Network(MLP(2, 10, 1));
 var myTrainer = new Trainer(myNet);
 Dataset.run().then(myDataset =>
-  myTrainer.train(myDataset, {
+  myTrainer.train(trainingSet, {
     rate: 0.1,
     iterations: 10000,
     error: 0.01,
@@ -94,39 +48,93 @@ Dataset.run().then(myDataset =>
 );
 
 io.on("connection", function(socket) {
+  socket.on("sendsensor", data => {
+    lastData[data.serial] = data.value;
+    let saida = predict(lastData.umd, lastData.luz);
+    console.log(data, `saindo: ${saida}`);
+    saida > 0.9
+      ? socket.emit("returnvalue", 1)
+      : saida < 0.1 && socket.emit("returnvalue", 0);
+  });
+  socket.on("getvalue", data => {
+    console.log("get value", data);
+    socket.emit("returnvalue", 1);
+  });
   console.log("a user is connected");
 });
 var port = process.env.PORT || 3001;
-
+http.listen(port, function() {
+  console.log("listening on *:" + port);
+});
 var lastData = {
-  luminosidade: 200,
-  umidade: 0,
-  valvula: 0
+  luz: 0.2,
+  umd: 0,
+  bmb: 0
 };
+
+// setInterval(() => {
+//   if (lastData.umd > 1) lastData.umd = 0;
+//   else lastData.umd += 0.05;
+
+//   predict(lastData.umd, lastData.luz);
+// }, 400);
+
+const predict = (umidade, luminosidade) =>
+  myNet.activate([umidade, luminosidade]);
+
+var trainingSet = [
+  {
+    input: [0, 200 / 1023],
+    output: [0]
+  },
+  {
+    input: [0.1, 200 / 1023],
+    output: [0]
+  },
+  {
+    input: [0.2, 200 / 1023],
+    output: [0]
+  },
+  {
+    input: [0.3, 200 / 1023],
+    output: [0.5]
+  },
+  {
+    input: [0.35, 200 / 1023],
+    output: [0.5]
+  },
+  {
+    input: [0.4, 200 / 1023],
+    output: [1]
+  },
+  {
+    input: [0.5, 200 / 1023],
+    output: [1]
+  },
+  {
+    input: [0.6, 200 / 1023],
+    output: [1]
+  },
+  {
+    input: [0.7, 200 / 1023],
+    output: [1]
+  },
+  {
+    input: [0.8, 200 / 1023],
+    output: [1]
+  },
+  {
+    input: [0.9, 200 / 1023],
+    output: [1]
+  },
+  {
+    input: [1, 200 / 1023],
+    output: [1]
+  }
+];
 // trainingSet.forEach(item =>
 //   Dataset.save({
 //     input: item.input,
 //     output: item.output
 //   }).then(data => console.log(data))
 // );
-
-setInterval(() => {
-  if (lastData.umidade > 1024 - 30) lastData.umidade = 0;
-  else lastData.umidade += 30;
-
-  predict(lastData.umidade, lastData.luminosidade);
-}, 400);
-
-function predict(umidade, luminosidade) {
-  let saida = myNet.activate([umidade / 1024, luminosidade / 1024])[0];
-  console.log(
-    `umi: ${(umidade / 1024).toFixed(2)}, lum: ${(luminosidade / 1024).toFixed(
-      2
-    )}, saída: ${saida.toFixed(2)}, conclusão: ${
-      Math.round(saida.toFixed(2)) ? "ligar" : "desligar"
-    } a bomba de agua`
-  );
-}
-
-// predict(400, 200);
-// predict(700, 200);
